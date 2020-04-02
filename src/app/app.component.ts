2899fc5a3b416a8bf33a1d160db276c1ceb744fe
yechangpeng666 <2201427581@qq.com>
2020-04-02 17:28:28 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'inventory-app';
+  title: string;
+  imgUrl: string;
+  url: string;
+
+  constructor() {
+    this.title = "hello"
+    this.imgUrl = "/assets/imgs/u=1238043994,1992604562&fm=26&gp=0.jpg";
+    this.url = "https://www.baidu.com";
+  }
+  onBtnClicked() {
+    console.log("哈哈哈")
+  }
+  onInput(evt: Event) {
+    this.title = (<HTMLInputElement>evt.target).value;
+  }
 }
